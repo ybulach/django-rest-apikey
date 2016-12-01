7239ffee3dec3f0e2f3515829cc4177a0e414e68
Yamine BULACH <ybulach@teclib.com>
2016-12-01 18:15:54 +0100

    Added migrations to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setup(
 	author = 'Yamine BULACH',
 	author_email = 'contact@yamine-bulach.eu',
 	url = 'https://github.com/ybulach/django-rest-apikey',
-	packages = ['django_rest_apikey'],
+	packages = [
+		'django_rest_apikey',
+		'django_rest_apikey.migrations'
+	],
 	install_requires = ['djangorestframework']
 )
