decf05228571c198a3e87756624f90fa3fceefb3
Yamine BULACH <ybulach@teclib.com>
2016-12-01 17:54:04 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+	name = 'django_rest_apikey',
+	description = 'Per-user API key to authenticate in Django REST Framework',
+	version = '0.1',
+	license = 'LGPL',
+	author = 'Yamine BULACH',
+	author_email = 'contact@yamine-bulach.eu',
+	url = 'https://github.com/ybulach/django-rest-apikey',
+	packages = ['django_rest_apikey'],
+	install_requires = ['djangorestframework']
+)
