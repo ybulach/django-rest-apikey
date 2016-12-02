a0c47ffdc76c9b67a8f921b3262499facb353345
Yamine BULACH <ybulach@teclib.com>
2016-12-02 11:25:14 +0100

    Added classifiers in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 setup(
 	name = 'django_rest_apikey',
 	description = 'Per-user API key to authenticate in Django REST Framework',
-	version = '0.1',
+	version = '0.1.0',
 	license = 'LGPL',
 	author = 'Yamine BULACH',
 	author_email = 'contact@yamine-bulach.eu',
@@ -18,5 +18,15 @@ setup(
 		'django_rest_apikey',
 		'django_rest_apikey.migrations'
 	],
-	install_requires = ['djangorestframework']
+	install_requires = ['djangorestframework'],
+	classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+    ],
 )
